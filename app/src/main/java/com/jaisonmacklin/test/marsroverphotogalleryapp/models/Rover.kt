ffd9d3868f1ec2ea3876b8bd9f5728c7a996b0b1
jaemak23 <macklin.jaison@gmail.com>
2022-06-07 18:04:02 +0530

    Created Rover.kt data class in models

---
+++ app/src/main/java/com/jaisonmacklin/test/marsroverphotogalleryapp/models/Rover.kt
@@ -0,0 +1,11 @@
+package com.jaisonmacklin.test.marsroverphotogalleryapp.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Rover(
+    @SerializedName("id") val id: Int,
+    @SerializedName("landing_date") val landingDate: String,
+    @SerializedName("launch_date") val launchDate: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("status") val status: String
+)
