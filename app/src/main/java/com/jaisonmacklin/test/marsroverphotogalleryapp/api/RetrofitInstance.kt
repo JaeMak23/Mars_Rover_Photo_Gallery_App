18ab2bdf5ca68c398e58baa2e92e0875b0ab1e2d
jaemak23 <macklin.jaison@gmail.com>
2022-06-07 18:42:14 +0530

    created RetrofitInstance.kt class in api package.

---
+++ app/src/main/java/com/jaisonmacklin/test/marsroverphotogalleryapp/api/RetrofitInstance.kt
@@ -0,0 +1,26 @@
+package com.jaisonmacklin.test.marsroverphotogalleryapp.api
+
+import com.jaisonmacklin.test.marsroverphotogalleryapp.R
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+        private const val BASE_URL = R.string.base_url.toString()
+
+        fun getRetrofitInstance(): Retrofit {
+            val logging = HttpLoggingInterceptor()
+            logging.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder()
+            client.addInterceptor(logging)
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
