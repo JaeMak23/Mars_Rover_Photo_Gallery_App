5d4da37f535bd4fa0dfc9e6e414922814d0db8f6
jaemak23 <macklin.jaison@gmail.com>
2022-06-07 17:36:51 +0530

    MainActivity.kt moved to views package.

--- app/src/main/java/com/jaisonmacklin/test/marsroverphotogalleryapp/views/MainActivity.kt
+++ app/src/main/java/com/jaisonmacklin/test/marsroverphotogalleryapp/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jaisonmacklin.test.marsroverphotogalleryapp
+package com.jaisonmacklin.test.marsroverphotogalleryapp.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jaisonmacklin.test.marsroverphotogalleryapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
